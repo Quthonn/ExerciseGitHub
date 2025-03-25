7ad8d05605f5e0f9d9770dd408e7905151ee9a2d
quthonn <gevorgyan.genri@gmail.com>
2025-03-26 00:03:16 +0700

    Gave a name to each step in the automated tests

--- src/test/java/CheckHaveElements.java
+++ src/test/java/CheckHaveElements.java
@@ -1,33 +1,71 @@
 import com.codeborne.selenide.*;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
 import org.openqa.selenium.By;
 
 public class CheckHaveElements {
 
     @Test
+    @DisplayName("Проверка имени первого участника репозитория")
+    @Feature("")
+    @Story("")
+    @Owner("Quthonn")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Link(value = "testing", url = "https://github.com")
     void solntsevShouldBeTheTopContributor() {
         Configuration.holdBrowserOpen = true;
-        open("https://github.com/selenide/selenide");
-        $(".BorderGrid").$(byText("Contributors")).ancestor(".BorderGrid-row")
-                .$$("ul li").first().hover();
-        $$(".Popover .Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
+        step("Открытие страницы GitHub", () -> {
+            open("https://github.com/selenide/selenide");
+        });
+
+        step("Открытие страницы GitHub", () -> {
+            $(".BorderGrid").$(byText("Contributors")).ancestor(".BorderGrid-row")
+                    .$$("ul li").first().hover();
+
+        });
+
+        step("Открытие страницы GitHub", () -> {
+            $$(".Popover .Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
+        });
 
     }
 
     @Test
+    @DisplayName("Проверка названия исходного кода в разделе Soft Assertions")
+    @Feature("")
+    @Story("")
+    @Owner("Quthonn")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Link(value = "testing", url = "https://github.com")
     void CheckHaveCode() {
         Configuration.holdBrowserOpen = true;
-        open("https://github.com/selenide/selenide");
-        $("[data-content=Wiki]").click();
-        $(".markdown-body").shouldHave(text("Soft assertions")).click();
-        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        step("Открытие страницы GitHub", () -> {
+            open("https://github.com/selenide/selenide");
+        });
 
-        $(By.cssSelector("html > body > div:nth-of-type(1) > div:nth-of-type(4) > div > main > turbo-frame > div > div > div:nth-of-type(3) > div > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(11) > pre"))
-                .shouldBe(visible); // Проверка, что элемент видим на странице
-        //последнюю строчку переделать сменив локатор на более читаемый на глаз
+        step("Переход в раздел Wiki", () -> {
+            $("[data-content=Wiki]").click();
+        });
 
+        step("Проверка наличия раздела Soft Asserrtions", () -> {
+            $(".markdown-body").shouldHave(text("Soft assertions")).click();
+        });
+
+        step("Переход в раздел Soft Asserrtions", () -> {
+            $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        });
+
+        step("Проверка наличия исходного кода", () -> {
+            $(By.cssSelector("html > body > div:nth-of-type(1) > div:nth-of-type(4) > div > main > turbo-frame > div > div > div:nth-of-type(3) > div > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(11) > pre"))
+                    .shouldBe(visible); // Проверка, что элемент видим на странице
+            //последнюю строчку переделать сменив локатор на более читаемый на глаз
+        });
     }
 }
--- src/test/java/openEnterpriseWindow.java
+++ src/test/java/openEnterpriseWindow.java
@@ -1,10 +1,13 @@
+import io.qameta.allure.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 public class openEnterpriseWindow {
     @BeforeAll
@@ -13,11 +16,35 @@ public class openEnterpriseWindow {
     }
 
     @Test
+    @DisplayName("Открытие раздела Enterprises и проверка его названия")
+    @Feature("")
+    @Story("")
+    @Owner("Quthonn")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Link(value = "testing", url = "https://github.com")
     void shouldFindSelenideRepositoryAtTheTop() {
-        open("https://github.com/");
-        $(By.cssSelector("html > body > div:nth-of-type(1) > div:nth-of-type(3) > header > div > div:nth-of-type(2) > div > nav > ul > li:nth-of-type(2) > button")).click();
-        $(By.cssSelector("a[data-analytics-event='{\"location\":\"navbar\",\"action\":\"enterprises\",\"context\":\"solutions\",\"tag\":\"link\",\"label\":\"enterprises_link_solutions_navbar\"}']")).click();
-        $(By.cssSelector("a[data-testid='SubNav-root-heading']")).shouldBe(text("Enterprise"));
+        step("Открытие страницы GitHub", () -> {
+            open("https://github.com/");
+        });
+
+        step("Открытие раздела Solutions", () -> {
+            $(By.cssSelector("html > body > div:nth-of-type(1) > " +
+                    "div:nth-of-type(3) > header > div > div:nth-of-type(2) > " +
+                    "div > nav > ul > li:nth-of-type(2) > button")).click();
+        });
+
+        step("Нажатие по кнопке Enterprises", () -> {
+            $(By.cssSelector("a[data-analytics-event='{\"location\":\"navbar\"," +
+                    "\"action\":\"enterprises\",\"context\":\"solutions\"," +
+                    "\"tag\":\"link\",\"label\":" +
+                    "\"enterprises_link_solutions_navbar\"}']")).click();
+        });
+
+        step("Проверка названия страницы", () -> {
+            $(By.cssSelector("a[data-testid='SubNav-root-heading']")).shouldBe(text("Enterprise"));
+        });
+
+
 
     }
 }
